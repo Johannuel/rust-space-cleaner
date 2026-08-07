38c962e605afc1860709182dc81b4afdbd1592bf
Johannuel <johannuelvmm@gmail.com>
2026-08-06 20:45:11 -0400

    test(clean): replace hardcoded paths with tempdir (portable)

--- tests/clean_safety.rs
+++ tests/clean_safety.rs
@@ -1,134 +1,145 @@
 #[path = "../src/clean.rs"]
 mod clean;
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use clean::is_safe_to_clean;
 
-fn whitelist_base() -> Vec<&'static Path> {
+fn make_whitelist(base: &Path) -> Vec<PathBuf> {
     vec![
-        Path::new("/home/mfcode/.cache"),
-        Path::new("/home/mfcode/.cache/cargo"),
-        Path::new("/home/mfcode/.cargo"),
-        Path::new("/home/mfcode/.rustup/tmp"),
-        Path::new("/home/mfcode/.npm/_cacache"),
+        base.join(".cache"),
+        base.join(".cache/cargo"),
+        base.join(".cargo"),
+        base.join(".rustup/tmp"),
+        base.join(".npm/_cacache"),
     ]
+}
+
+fn to_refs<P: AsRef<Path>>(paths: &[P]) -> Vec<&Path> {
+    paths.iter().map(|p| p.as_ref()).collect()
 }
 
 #[test]
 fn aprueba_ruta_exacta_de_la_whitelist() {
-    let whitelist = whitelist_base();
+    let tmp = tempfile::tempdir().unwrap();
+    let paths = make_whitelist(tmp.path());
+    let whitelist = to_refs(&paths);
     assert!(is_safe_to_clean(
-        Path::new("/home/mfcode/.cache/cargo"),
+        &tmp.path().join(".cache/cargo"),
         &whitelist
     ));
     assert!(is_safe_to_clean(
-        Path::new("/home/mfcode/.rustup/tmp"),
+        &tmp.path().join(".rustup/tmp"),
         &whitelist
     ));
     assert!(is_safe_to_clean(
-        Path::new("/home/mfcode/.npm/_cacache"),
+        &tmp.path().join(".npm/_cacache"),
         &whitelist
     ));
 }
 
 #[test]
 fn aprueba_subdirectorio_directo_de_cache() {
-    let whitelist = whitelist_base();
+    let tmp = tempfile::tempdir().unwrap();
+    let paths = make_whitelist(tmp.path());
+    let whitelist = to_refs(&paths);
     assert!(is_safe_to_clean(
-        Path::new("/home/mfcode/.cache/paru"),
+        &tmp.path().join(".cache/paru"),
         &whitelist
     ));
-    assert!(is_safe_to_clean(
-        Path::new("/home/mfcode/.cache/pip"),
-        &whitelist
-    ));
+    assert!(is_safe_to_clean(&tmp.path().join(".cache/pip"), &whitelist));
 }
 
 #[test]
 fn rechaza_subdirectorio_anidado_de_cache() {
-    let whitelist = whitelist_base();
+    let tmp = tempfile::tempdir().unwrap();
+    let paths = make_whitelist(tmp.path());
+    let whitelist = to_refs(&paths);
     assert!(!is_safe_to_clean(
-        Path::new("/home/mfcode/.cache/paru/tmp"),
+        &tmp.path().join(".cache/paru/tmp"),
         &whitelist
     ));
 }
 
 #[test]
 fn rechaza_todo_cache_a_secas() {
-    let whitelist = whitelist_base();
-    assert!(!is_safe_to_clean(
-        Path::new("/home/mfcode/.cache"),
-        &whitelist
-    ));
+    let tmp = tempfile::tempdir().unwrap();
+    let paths = make_whitelist(tmp.path());
+    let whitelist = to_refs(&paths);
+    assert!(!is_safe_to_clean(&tmp.path().join(".cache"), &whitelist));
 }
 
 #[test]
 fn rechaza_prefijo_falso_de_cargo() {
-    let whitelist = whitelist_base();
+    let tmp = tempfile::tempdir().unwrap();
+    let paths = make_whitelist(tmp.path());
+    let whitelist = to_refs(&paths);
     assert!(!is_safe_to_clean(
-        Path::new("/home/mfcode/.cargo_evil"),
+        &tmp.path().join(".cargo_evil"),
         &whitelist
     ));
     assert!(!is_safe_to_clean(
-        Path::new("/home/mfcode/.cacheevil"),
+        &tmp.path().join(".cacheevil"),
         &whitelist
     ));
 }
 
 #[test]
 fn rechaza_prefijo_falso_anidado() {
-    let whitelist = whitelist_base();
+    let tmp = tempfile::tempdir().unwrap();
+    let paths = make_whitelist(tmp.path());
+    let whitelist = to_refs(&paths);
     assert!(!is_safe_to_clean(
-        Path::new("/home/mfcode/.rustup/tmp_evil"),
+        &tmp.path().join(".rustup/tmp_evil"),
         &whitelist
     ));
     assert!(!is_safe_to_clean(
-        Path::new("/home/mfcode/.rustup/tmp/otra"),
+        &tmp.path().join(".rustup/tmp/otra"),
         &whitelist
     ));
 }
 
 #[test]
 fn rechaza_rutas_fuera_de_la_whitelist() {
-    let whitelist = whitelist_base();
+    let tmp = tempfile::tempdir().unwrap();
+    let paths = make_whitelist(tmp.path());
+    let whitelist = to_refs(&paths);
+    assert!(!is_safe_to_clean(&tmp.path().join("Documents"), &whitelist));
     assert!(!is_safe_to_clean(Path::new("/etc"), &whitelist));
-    assert!(!is_safe_to_clean(
-        Path::new("/home/mfcode/Documents"),
-        &whitelist
-    ));
-    assert!(!is_safe_to_clean(Path::new("/tmp"), &whitelist));
+    assert!(!is_safe_to_clean(Path::new("/"), &whitelist));
 }
 
 #[test]
 fn rechaza_home_y_raiz() {
-    let whitelist = whitelist_base();
-    assert!(!is_safe_to_clean(Path::new("/home/mfcode"), &whitelist));
+    let tmp = tempfile::tempdir().unwrap();
+    let paths = make_whitelist(tmp.path());
+    let whitelist = to_refs(&paths);
+    assert!(!is_safe_to_clean(tmp.path(), &whitelist));
     assert!(!is_safe_to_clean(Path::new("/"), &whitelist));
 }
 
 #[test]
 fn normaliza_puntos_y_barra_final() {
-    let whitelist = whitelist_base();
+    let tmp = tempfile::tempdir().unwrap();
+    let paths = make_whitelist(tmp.path());
+    let whitelist = to_refs(&paths);
     assert!(is_safe_to_clean(
-        Path::new("/home/mfcode/.cache/cargo/"),
+        &tmp.path().join(".cache/cargo/"),
         &whitelist
     ));
     assert!(is_safe_to_clean(
-        Path::new("/home/mfcode/.cache/./cargo"),
+        &tmp.path().join(".cache/./cargo"),
         &whitelist
     ));
     assert!(!is_safe_to_clean(
-        Path::new("/home/mfcode/.cache/../.cache"),
+        &tmp.path().join(".cache/../.cache"),
         &whitelist
     ));
 }
 
 #[test]
 fn whitelist_vacia_rechaza_todo() {
-    let whitelist: Vec<&Path> = vec![];
-    assert!(!is_safe_to_clean(
-        Path::new("/home/mfcode/.cache/cargo"),
-        &whitelist
-    ));
+    let empty: Vec<&Path> = vec![];
+    let tmp = tempfile::tempdir().unwrap();
+    assert!(!is_safe_to_clean(&tmp.path().join(".cache/cargo"), &empty));
 }
